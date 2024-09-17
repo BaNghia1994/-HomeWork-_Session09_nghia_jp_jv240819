faec01a2af7b963bcfabb87083adc2a38cde2d6c
BaNghia1994 <nghiacao2494@gmail.com>
2024-09-18 02:21:28 +0900

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,108 @@
+//exercise 1
+//let birthYear = prompt("Bạn sinh năm bao nhiêu:");
+//let curentYear = 2024;
+//if (birthYear<=2024) {
+// let age = 2024 - parseInt(birthYear);
+//  console.log(age);
+//}else{
+//console.log("Giá trị không hợp lệ:");
+//}
+
+//EX2
+//let number = +prompt("MỜi bận nhập số bất kì:");
+////let so = parseInt(number);
+//if(so % 2 ===0){
+// alert("là số  chẵn:");
+//}else if (so % 2 ===1){
+// alert("là số lẻ:");
+//}else{
+// alert("Không hợp lệ:");
+// }
+
+
+//EX3
+//let day = prompt("Mời bạn nhập một số bất kì (2 đến 8)");
+//let so = parseInt(day);
+//switch (so) {
+// case 2:
+////    alert("Monday");
+//   break;
+//case 3:
+//  alert("Tuesday");
+//  break;
+//case 4:
+//   alert("Wednesday");
+//   break;
+//case 5:
+//   alert("Thursday");
+//   break;
+//case 6:
+//  alert("Friday");
+//  break;
+//case 7:
+//  alert("Saturday");
+// break;
+//case 8:
+//alert("Sunday");
+//  break;
+//default:
+// alert("Giá trị không hợp lệ.");
+//}
+
+//EX4
+
+//let number = +prompt("Nhập vào một số bất kì:");
+//let so = parseInt(number);
+//if (so > 0){
+// alert("Số dương:");
+//}else if(so < 0){
+// alert("Số âm:");
+//}else{
+// alert("Bằng 0:");
+//}
+
+//EX5
+
+//let n = prompt("Mời bạn nhập một số nguyên dương bất kỳ:");
+//let number = parseInt(n);
+//if(number <= 0){
+//console.log("Vui lòng nhập số nguyên dương:");
+//}else{
+ //for(let i = 1; i <= number; i++){
+   // console.log(i);
+
+ //}
+//}
+
+//EX6
+let input = +prompt("MỜi bạn nhập một số nguyên dương:");
+let number = 1;
+if(number < 0){
+    console.log("Vui lòng nhập số nguyên dương:");
+}else{for( i = 1; i <= input; i++){
+   number = number*i;}
+
+} 
+console.log(number);
+
+
+
+ 
+
+
+
+//EX7
+ //for( let i = 1; i <= 100; i++){
+   // if(i % 3 === 0){
+    //    console.log("Fizz");
+    //}else if(i % 5 ===0){
+    //    console.log("Buzz");
+    // }else if(i % 15 ===0){
+    //    console.log("FizzBuzz");
+    // }else{
+    //    console.log(i);
+    // }
+    
+ //}
+
+
